6b3011ed69e1497241ef88fe7ad413bec3015e88
Your Name <you@example.com>
2022-03-18 01:58:40 +0530

    First commit

---
+++ LockedMe/src/LockedMeMain.java
@@ -0,0 +1,14 @@
+
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+		
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedMe", "Sriraksha");
+		
+		HandleOptions.handleWelcomeScreenInput();
+
+	}
+
+}
